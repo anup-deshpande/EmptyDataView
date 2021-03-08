84f12ffffe55523fa51c86bcd208f0a3fcd0234d
Anup Deshpande <deshpande.anup24@gmail.com>
2021-03-08 15:57:45 -0500

    feat: add demo project

---
+++ EmptyDataView-Demo/Data Source/Colors.swift
@@ -0,0 +1,26 @@
+//
+//  Colors.swift
+//  EmptyDataView-Demo
+//
+//  Created by Anup Deshpande on 3/8/21.
+//
+
+import Foundation
+
+class ColorData {
+    class func getColors() -> [String]{
+        let colors = ["Black",
+                      "Blue",
+                      "Brown",
+                      "Green",
+                      "Indigo",
+                      "Orange",
+                      "Pink",
+                      "Purple",
+                      "Red",
+                      "Red",
+                      "White",
+                      "Yellow"]
+        return colors
+    }
+}
